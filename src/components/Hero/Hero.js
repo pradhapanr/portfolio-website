603a25bd4aa34fd286c9e167037d4aa9eb4359f3
Pradhapan Rajendran <rajendranpradhapan@gmail.com>
2021-07-24 05:45:06 -0400

    initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection, IgnoreCSS } from './HeroStyles';
+
+const Hero = () => (
+    <div>
+        <Section row nopadding>
+            <LeftSection>
+                <SectionTitle main center>
+                    Hey there! <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Emoji_u1f44b.svg/1200px-Emoji_u1f44b.svg.png" width="60" height="60"/><br />
+                    My names Prad.
+                </SectionTitle>
+                <SectionText>
+                    I am a student at the University of Guelph going into my third year. 
+                </SectionText>
+                <Button onClick = {() => window.location = "https://google.com"}>
+                    Learn More
+                </Button>
+            </LeftSection>
+        </Section>
+    </div>
+);
+
+export default Hero;
